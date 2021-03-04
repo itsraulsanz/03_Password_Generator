7590f137bfba0cd9fdd374bff4889b654319af11
Raul Sanz <luar_znas@hotmail.com>
2021-03-04 23:35:05 +0000

    userLength function

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Variables
+var userLength = "";
+var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+var specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{\|}~";
 
 
 function generatePassword() {
-    var strength = 0;
-    var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
-    var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var numbers = "1234567890";
-    var specialCharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
     var characterOptionsForFinalPassword = "";
     var finalPassword = "";
 
-    var userWantsStrength = window.prompt("How many characters would you like in your password?")
-    var userWantsUpperLetters = confirm("Would you like Upper Case Letters in your password?")
+    userLength = prompt("How many characters would you like in your password?");
+
+    if (userLength < 8) {
+        alert("Password must contains between 8 and 128 characters");
+        return "";
+    } else if (userLength > 128) {
+        alert("Password must contains between 8 and 128 characters");
+        return "";
+    } else {
+        userWantsLowerLetters;
+    }
+
+
     var userWantsLowerLetters = confirm("Would you like Lower Case Letters in your password?")
     var userWantsNumbers = confirm("Would you like Upper Case Letters in your password?")
     var userWantsSpecialCharacters = confirm("Would you like Lower Case Letters in your password?")
 
-    if (userWantsStrength === true) {
-        characterOptionsForFinalPassword += strength;
-    }
+    /*
 
     if (userWantsUpperLetters === true) {
         characterOptionsForFinalPassword += upperCaseLetters;
@@ -38,11 +48,62 @@ function generatePassword() {
     if (userWantsSpecialCharacters === true) {
         characterOptionsForFinalPassword += specialCharacters;
     }
+*/
 
     return characterOptionsForFinalPassword
 
     // return "Congrats new password is here!"
 }
+
+
+
+/*
+//var characterOptionsForFinalPassword = "";
+//var finalPassword = "";
+
+function userLength() {
+
+    passLength = prompt("How many characters would you like in your password? (Between 8 and 128 characters)");
+
+    if (passLength < 8) {
+        alert("Password must contains between 8 and 128 characters");
+        userLength;
+    } else if (passLength > 128) {
+        alert("Password must contains between 8 and 128 characters");
+        userLength;
+    }
+    return passLength;
+}
+
+//var userWantsUpperLetters = confirm("Would you like Upper Case Letters in your password?")
+//var userWantsLowerLetters = confirm("Would you like Lower Case Letters in your password?")
+//var userWantsNumbers = confirm("Would you like Upper Case Letters in your password?")
+//var userWantsSpecialCharacters = confirm("Would you like Lower Case Letters in your password?")
+
+
+
+if (userWantsUpperLetters === true) {
+    characterOptionsForFinalPassword += upperCaseLetters;
+}
+
+if (userWantsLowerLetters === true) {
+    characterOptionsForFinalPassword += lowerCaseLetters;
+}
+
+if (userWantsNumbers === true) {
+    characterOptionsForFinalPassword += numbers;
+}
+
+if (userWantsSpecialCharacters === true) {
+    characterOptionsForFinalPassword += specialCharacters;
+}
+
+//return characterOptionsForFinalPassword
+
+// return "Congrats new password is here!"
+
+*/
+
 
 // Write password to the #password input
 function writePassword() {
